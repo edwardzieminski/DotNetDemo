ffa5a1f98893b59be3ec492443095fa47e8e6c17
Edward Zieminski <edward.zieminski@gmail.com>
2020-11-30 23:18:56 +0100

    Coding Tree meeting

---
+++ ConsleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Models;
+
+namespace ConsleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new PersonModel()
+            {
+                FirstName = "Edward",
+                LastName = "Ziemiński"
+            };
+
+            var przywitanie = $@"Hello { model.FirstName } { model.LastName }
+            fgdfg gfdsgdf";
+            
+            Console.WriteLine(przywitanie);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Models
+{
+    public class PersonModel
+    {
+        private string _firstName;
+
+        public PersonModel()
+        {
+            
+        }
+        
+        public string FirstName 
+        {
+            get 
+            {
+                return _firstName;
+            }
+            set 
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName { get; set; }
+
+        public string GetFirstName() 
+        {
+            return _firstName;
+        }
+    }
+}
